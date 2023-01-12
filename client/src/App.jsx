f9169b07ae00332e34a4f8ea9d5bf0ea69247118
Ritick Chaudhary <ritick20460@iiitd.ac.in>
2023-01-12 15:30:50 +0530

    navbar designed, for full screen and mobile devices

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
         <Sidebar />
       </div>
 
-      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+      <div className='flex-1 max-sm:w-full w-full mx-auto sm:pr-5'>
         <Navbar />
         
         <Routes>
