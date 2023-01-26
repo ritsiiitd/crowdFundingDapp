8de1ed7afecf6a25850c334ff718e8041c0273c7
Ritick Chaudhary <ritick20460@iiitd.ac.in>
2023-01-26 18:19:54 +0530

    loader visible

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,3 +5,4 @@ export { default as FormField } from './FormField';
 export { default as DisplayCampaign } from './DisplayCampaign';
 export { default as Card } from './Card';
 export { default as CountBox } from './CountBox';
+export { default as Loader } from './Loader';
